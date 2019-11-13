c7d4ee8789a5ff11debda8430121e040584ff4c2
VeraDidenko <verochka1998@gmail.com>
2019-11-13 19:18:24 +0200

    Added vue-flexmonster component

---
+++ src/Pivot.vue
@@ -0,0 +1,70 @@
+<!--HTML related information for displaying the component-->
+<template>
+  <div>Pivot</div>
+</template>
+
+<!--JavaScript related information for the component's logic-->
+<script>
+import Flexmonster from "flexmonster";
+export default {
+  props: {
+    afterchartdraw: Function,
+    aftergriddraw: Function,
+    beforegriddraw: Function,
+    beforetoolbarcreated: Function,
+    cellclick: Function,
+    celldoubleclick: Function,
+    componentFolder: String,
+    customizeCell: Function,
+    customizeContextMenu: Function,
+    datachanged: Function,
+    dataerror: Function,
+    datafilecancelled: Function,
+    dataloaded: Function,
+    fieldslistclose: Function,
+    fieldslistopen: Function,
+    filterclose: Function,
+    filteropen: Function,
+    fullscreen: Function,
+    global: Object,
+    height: [String, Number],
+    licenseKey: String,
+    loadingdata: Function,
+    loadinglocalization: Function,
+    loadingolapstructure: Function,
+    loadingreportfile: Function,
+    localizationerror: Function,
+    localizationloaded: Function,
+    olapstructureerror: Function,
+    olapstructureloaded: Function,
+    openingreportfile: Function,
+    querycomplete: Function,
+    queryerror: Function,
+    ready: Function,
+    report: [String, Object],
+    reportchange: Function,
+    reportcomplete: Function,
+    reportfilecancelled: Function,
+    reportfileerror: Function,
+    reportfileloaded: Function,
+    runningquery: Function,
+    toolbar: Boolean,
+    update: Function,
+    width: [Number, String]
+  },
+  mounted: function() {
+    this.flexmonster = new Flexmonster({
+      ...this.$props,
+      container: this.$el
+    });
+  },
+  beforeUpdate() {
+    return false;
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "~flexmonster/flexmonster.min.css";
+</style>
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Pivot from "./Pivot.vue";
+
+export default {
+ install(Vue, options) {
+  Vue.component("Pivot", Pivot);
+ }
+};

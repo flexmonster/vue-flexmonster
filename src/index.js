a4480ad631449444fd4687da4553454105bc6cfa
VeraDidenko <verochka1998@gmail.com>
2019-11-29 20:33:13 +0200

    fixed dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import Pivot from "./Pivot.vue";
+import Vue from 'vue'
+import Pivot from './Pivot.vue';
 
-export default {
- install(Vue, options) {
-  Vue.component("Pivot", Pivot);
- }
-};
+//Allows user to set a custom component name
+function install(Vue, options = {}){
+   Vue.component(options.PivotName || 'Pivot', Pivot)
+}
+
+//Allows user to use Pivot as a plugin
+export default install
+
+//Auto-Install when included directly in the browser:
+if(typeof window != undefined && window.Vue && window.Vue == Vue){
+ install(window.Vue)
+}
+
+//Allows user to register Pivot locally, without global registration
+export {
+  Pivot
+}
